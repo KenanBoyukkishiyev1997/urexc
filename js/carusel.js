3e203301d26273a000dc65cf436b02f13d762032
KenanBoyukkishiyev1997 <boyukkishiyevk@gmail.com>
2023-07-17 10:18:46 +0400

    first commit

---
+++ js/carusel.js
@@ -0,0 +1,45 @@
+export function carusel() {
+  $(document).ready(function () {
+    $(".sliderCarusel").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        autoplay: true,
+        focusOnSelect: true,
+        draggable: true,
+        arrows: false,
+        swipe: true, // Enable swipe/drag effect
+        touchMove: true,
+        touchThreshold: 50, // Set a higher value for touchThreshold
+        autoplaySpeed:1000, //  Slide Delay
+        speed:800,
+        easing:"linear",
+        lazyLoad: 'progressive',
+        // variableWidth: true,
+        // centerMode: true,
+        responsive: [
+            {
+              breakpoint: 1280,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+                breakpoint: 500,
+                settings: {
+                  slidesToShow: 1
+                }
+              }
+          ]
+
+    });
+  });
+}
