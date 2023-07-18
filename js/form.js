d371d19362c5bdfd0e129a93613deb021fe5033e
KenanBoyukkishiyev1997 <boyukkishiyevk@gmail.com>
2023-07-18 21:18:27 +0400

    option

--- js/form.js
+++ js/form.js
@@ -1,25 +1,27 @@
 export function form(){
-    function handleInputKeyDown(event) {
-        if (event.key === 'Enter') {
-          const currentInput = event.target;
-          const nextInput = getNextInput(currentInput);
-      
-          if (nextInput) {
-            event.preventDefault(); // Prevent form submission
-            nextInput.focus(); // Focus on the next input field
-          }
-        }
-      }
-      
-      function getNextInput(currentInput) {
-        const inputs = document.querySelectorAll('.contact__form-input');
-        const currentIndex = Array.from(inputs).indexOf(currentInput);
-        const nextIndex = currentIndex + 1;
-      
-        if (nextIndex < inputs.length) {
-          return inputs[nextIndex];
-        }
-      
-        return null; // Return null if there are no more input fields
-      }
-}
+    function handleDropdownChange() {
+        const dropdown = document.getElementById('dropdown');
+const dropdownFirst = document.getElementById('dropdownFirst');
+        dropdown.addEventListener('change', (e) => {
+            [...e.target.children].forEach(element => {
+                if (element.selected) e.target.classList.add('valid-option');
+            });
+            
+        })
+
+        dropdownFirst.addEventListener('change', (e) => {
+            [...e.target.children].forEach(element => {
+                if (element.selected) e.target.classList.add('valid-option');
+            });
+            
+        })
+    }
+    handleDropdownChange()
+
+    }
+
+
+
+
+
+
--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
 import { carusel } from './carusel.js';
- import {form} from './form.js'
+import {form} from './form.js'
 import {scrollHeader} from './scrollHeader.js'
+
 carusel();
 scrollHeader()
  form()
 
-
-// let options = {searchable: true, placeholder: 'select', searchtext: 'search', selectedtext: 'geselecteerd'};
-// NiceSelect.bind(document.getElementById("seachable-select"), options);
 
 
 
@@ -20,3 +18,6 @@ text.innerHTML = text.innerText
 	)
 	.join("");
 
+
+
+    
