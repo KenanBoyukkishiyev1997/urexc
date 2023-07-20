7a8fbd0c7c9e0b53c0696cf5d203d48ae3c2ec1f
KenanBoyukkishiyev1997 <boyukkishiyevk@gmail.com>
2023-07-20 13:09:38 +0400

    opaciti select input

--- js/form.js
+++ js/form.js
@@ -1,72 +1,48 @@
 export function form() {
-//   function handleDropdownChange() {
-//     const dropdown = document.getElementById("dropdown");
-//     const dropdownFirst = document.getElementById("dropdownFirst");
-//     // dropdown.addEventListener("change", (e) => {
-//     //   [...e.target.children].forEach((element) => {
-//     //     if (element.selected) e.target.classList.add("valid-option");
-//     //   });
-//     // });
-
-//     // dropdownFirst.addEventListener('change', (e) => {
-//     //     [...e.target.children].forEach(element => {
-//     //         if (element.selected) e.target.classList.add('valid-option');
-//     //     });
-
-//     // })
-//   }
-//   handleDropdownChange();
-
-document.addEventListener("DOMContentLoaded", function() {
-    const customSelects = document.querySelectorAll(".custom-select");
+    document.addEventListener("DOMContentLoaded", function() {
+      const customSelects = document.querySelectorAll(".custom-select");
   
-    customSelects.forEach(function(select) {
-      const label = select.querySelector(".select-label");
-      const optionsList = select.querySelector(".options");
-      const inputHidden = select.querySelector("input[type='hidden']");
-      let isOpen = false;
+      customSelects.forEach(function(select) {
+        const label = select.querySelector(".select-label");
+        const optionsList = select.querySelector(".options");
+        const inputHidden = select.querySelector("input[type='hidden']");
+        let isOpen = false;
   
-      select.addEventListener("click", function(event) {
-        event.stopPropagation();
-        isOpen = !isOpen;
-        toggleOptionsList();
+        select.addEventListener("click", function(event) {
+          event.stopPropagation();
+          isOpen = !isOpen;
+          toggleOptionsList();
+        });
+  
+        const options = optionsList.querySelectorAll("li");
+  
+        options.forEach(function(option) {
+          option.addEventListener("click", function(event) {
+            event.stopPropagation();
+            label.style.color = 'black';
+            label.textContent = option.textContent;
+            inputHidden.value = option.getAttribute("data-value");
+            isOpen = false;
+            toggleOptionsList();
+          });
+        });
+  
+        function toggleOptionsList() {
+          if (isOpen) {
+            optionsList.classList.add("show"); // Add the "show" class to display the list
+          } else {
+            optionsList.classList.remove("show"); // Remove the "show" class to hide the list
+          }
+        }
       });
   
-      const options = optionsList.querySelectorAll("li");
-  
-      options.forEach(function(option) {
-
-        option.addEventListener("click", function(event) {
-          event.stopPropagation();
-        label.style.color='black'
-
-          label.textContent = option.textContent;
-          inputHidden.value = option.getAttribute("data-value");
-          isOpen = false;
-          toggleOptionsList();
-
-
+      // Close the dropdown when clicking outside the custom select
+      document.addEventListener("click", function(event) {
+        customSelects.forEach(function(select) {
+          const optionsList = select.querySelector(".options");
+          optionsList.classList.remove("show"); // Always hide the list when clicking outside
         });
       });
-  
-      function toggleOptionsList() {
-        if (isOpen) {
-          optionsList.style.display = "block";
-        } else {
-          optionsList.style.display = "none";
-        }
-      }
     });
+  }
   
-    // Close the dropdown when clicking outside the custom select
-    document.addEventListener("click", function(event) {
-      customSelects.forEach(function(select) {
-        const optionsList = select.querySelector(".options");
-        optionsList.style.display = "none";
-      });
-    });
-  });
-  
-}
-
-
